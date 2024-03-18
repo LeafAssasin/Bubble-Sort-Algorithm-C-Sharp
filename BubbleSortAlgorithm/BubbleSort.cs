a8b94240b6cc1ca5030f700bc829fcc52c65c709
Mitko Mitkov <mitkomitkov2727@gmail.com>
2024-03-18 18:48:29 +0200

    Add project files.

---
+++ BubbleSortAlgorithm/BubbleSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortAlgorithm
+{
+    public static class BubbleSort<T> where T : IComparable<T>
+    {
+        public static T[] SortAscending(T[] arr)
+        {
+            int arrLength = arr.Length;
+
+            for (int i = 0; i < arrLength - 1; i++)
+            {
+                for (int j = 0; j < arrLength - i - 1; j++)
+                {
+                    //arr[j] > arr[j + 1]
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        T temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public static T[] SortDescending(T[] arr)
+        {
+            int arrLength = arr.Length;
+
+            for (int i = 0; i < arrLength - 1; i++)
+            {
+                for (int j = 0; j < arrLength - i - 1; j++)
+                {
+                    //arr[j] < arr[j + 1]
+                    if (arr[j].CompareTo(arr[j + 1]) < 0)
+                    {
+                        T temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ BubbleSortAlgorithm/Program.cs
@@ -0,0 +1,106 @@
+﻿using BubbleSortAlgorithm;
+using System;
+using System.Diagnostics;
+using bubbleInt = BubbleSortAlgorithm.BubbleSort<int>;
+using bubbleDouble = BubbleSortAlgorithm.BubbleSort<double>;
+using bubbleString = BubbleSortAlgorithm.BubbleSort<string>;
+
+namespace BubbleSortAlgorithm
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static int n = 15;
+
+        //TODO: Add Unit tests
+        static void Main(string[] args)
+        {
+            int[] testIntArr = GenerateIntArray();
+            double[] testDoubleArr = GenerateDoubleArray();
+            string[] testStringArr = GenerateStringArray();
+
+            Console.WriteLine("Original int arr:");
+            Console.WriteLine(String.Join(", ", testIntArr));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Original double arr:");
+            Console.WriteLine(String.Join(", ", testDoubleArr));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Original string arr:");
+            Console.WriteLine(String.Join(", ", testStringArr));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Int ascending:");
+            Console.WriteLine(String.Join(", ", bubbleInt.SortAscending(testIntArr)));
+            Console.WriteLine();
+            Console.WriteLine("Int descending:");
+            Console.WriteLine(String.Join(", ", bubbleInt.SortDescending(testIntArr)));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Double ascending:");
+            Console.WriteLine(String.Join(", ", bubbleDouble.SortAscending(testDoubleArr)));
+            Console.WriteLine();
+            Console.WriteLine("Double descending:");
+            Console.WriteLine(String.Join(", ", bubbleDouble.SortDescending(testDoubleArr)));
+
+            Console.WriteLine();
+
+            Console.WriteLine("String ascending:");
+            Console.WriteLine(String.Join(", ", bubbleString.SortAscending(testStringArr)));
+            Console.WriteLine();
+            Console.WriteLine("String descending:");
+            Console.WriteLine(String.Join(", ", bubbleString.SortDescending(testStringArr)));
+        }
+
+        static int[] GenerateIntArray()
+        {
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rand.Next(0, 101);
+            }
+
+            return arr;
+        }
+
+        static double[] GenerateDoubleArray()
+        {
+            double[] arr = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rand.Next(0, 101);
+            }
+
+            return arr;
+        }
+
+        static string[] GenerateStringArray()
+        {
+            string[] arr = new string[n];
+
+            for (int i = 0;i < n; i++)
+            {
+                int lenght = rand.Next(1, 21);
+
+                arr[i] = RandomString(lenght);
+            }
+
+            return arr;
+        }
+
+        //generates random strings
+        static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[rand.Next(s.Length)]).ToArray());
+        }
+    }
+}
